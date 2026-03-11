fa0a197a449642546319b80f5c4b74bfe735387d
Trouffman <trouffman@gmail.com>
2026-03-11 12:14:47 -0400

    cannot use version on non-versionned libndi URL

--- Casks/libndi.rb
+++ Casks/libndi.rb
@@ -1,6 +1,5 @@
 cask "libndi" do
-  version "6.3.1.0"
-  sha256 "0bba842f9d8323de2ce1c57d229588dae84bbb838be4092be9514830331d8d1d"
+  version "latest"
 
   url "https://downloads.ndi.tv/SDK/NDI_SDK_Mac/libNDI_for_Mac.pkg",
       verified: "downloads.ndi.tv/SDK/NDI_SDK_Mac/"
